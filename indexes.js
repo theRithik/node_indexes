7cc798d205b34ad5affb267bfbdddd1157252a2c
rithik <115656594+theRithik@users.noreply.github.com>
2023-04-10 23:20:17 +0530

    Update indexes.js

--- indexes.js
+++ indexes.js
@@ -3,3 +3,10 @@ db.emp.insert({emp_id:1002,name:"ankit",lastname:"bajpai",email:"ankit@google.co
 db.emp.insert({emp_id:1003,name:"swati",lastname:"singla",email:"swati@google.com",phone:9745261738,Job_id:'E4',sal:60000,manager_id:'104',arr:['a','b','c','d']})
 db.emp.insert({emp_id:1004,name:"yash",lastname:"sharma",email:"yash@google.com",phone:98745614455,Job_id:'E3',sal:77000,manager_id:'102'})
 db.emp.insert({emp_id:1005,name:"alaykhyaa",lastname:"bajpai",email:"alaykhyaa@google.com",phone:98526173999,Job_id:'E4',sal:89000,manager_id:'103'})
+
+
+db.emp.getIndexes()
+
+db.emp.createIndexes({"job_id":1})
+
+db.emp.dropIndexes({"job_id":1})
